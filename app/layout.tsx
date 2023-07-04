45c2a45db37091b3706018a4b22d262afb55d4ba
willian-onuki <willianonuki1@gmail.com>
2023-07-04 11:20:27 -0300

    feat: header created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Trello 2.0',
@@ -15,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='bg-[#F5F6F8]'>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <h1>hello world</h1>
+    <main>
+      <Header />
     </main>
-  )
+  );
 }
